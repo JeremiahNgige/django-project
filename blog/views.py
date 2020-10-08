c918cb9ef211a013d5cacdd72dda2c00c1d3b127
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-10-08 23:26:22 +0300

    django sqlite db

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author':'Jeremiha',
+        'title':'Django',
+        'content':'First time using django',
+        'date_posted':'25-5-2020', 
+    },
+    {
+        'author':'Anita',
+        'title':'Django',
+        'content':'Django on the way',
+        'date_posted':'25-5-2020',
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request,'blog/about.html')
