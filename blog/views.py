5b2fa859b941e13605baaa195f6c88dee24b54c1
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-10-12 11:56:26 +0300

    folder structure

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author':'Jeremiha',
-        'title':'Django',
-        'content':'First time using django',
-        'date_posted':'25-5-2020', 
-    },
-    {
-        'author':'Anita',
-        'title':'Django',
-        'content':'Django on the way',
-        'date_posted':'25-5-2020',
-    }
-]
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY ='y4h7=oeeq@bo*uz_l#f#$8+_7nqae075zm6)evs$*tc2%j43o%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
